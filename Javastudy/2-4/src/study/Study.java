4044473d629f090dce5dd3e5ba935ca27a2d5b24
koheisuzuki0412 <68722529+koheisuzuki0412@users.noreply.github.com>
2021-02-03 18:50:56 +0900

    課題(再提出)：2-4
    パッケージ

--- Javastudy/2-4/src/study/Study.java
+++ Javastudy/2-4/src/study/Study.java
@@ -1,8 +1,9 @@
 package study;
 //①FruitsConstants.javaをインポートしなさい。
-import constants.FruitsConstants;
+//import constants.FruitsConstants;
 //②Fruits.javaをインポートしなさい。
 import fruits.Fruits;
+
 /**
  * 本課題では、基本的なJavaの構造と、importについて理解を深めていきましょう。
  *
@@ -18,14 +19,16 @@ public class Study {
          *     また、課題の画像と同じ表示になるように
          *     printFruitsの引数にはFruitsConstantsから呼び出した値を設定してください。
          */
+    	Fruits fruits = new Fruits();
+    	Fruits.printFruits("レモン", 20);
+//    	System.out.println();
+    	//    	printFruits("レモン", 20);
+//    	Fruits.printFruits(x, y);
 
-    	FruitsConstants fruits = new FruitsConstants();
-    	String x = fruits.FRUITS_LEMON_04;
-
-    	FruitsConstants count = new FruitsConstants();
-    	Integer y = count.FRUITS_COUNT_20;
-
-    	Fruits.printFruits(x, y);
+//    	FruitsConstants fruits = new FruitsConstants();
+//    	String x = fruits.FRUITS_LEMON_04;
+//    	FruitsConstants count = new FruitsConstants();
+//    	Integer y = count.FRUITS_COUNT_20;
 
 //    	Fruits.printFruits(FruitsConstants("レモン", 20));
 //    	System.out.println(printFruits(FruitsConstants("レモン",20)));
@@ -33,4 +36,5 @@ public class Study {
 //    	Fruits.printFruits("レモン", 20);
 //    	System.out.println(printFruits);
     }
+
 }
